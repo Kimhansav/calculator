d89082f07665ad3da959a8d0429672a35e41adf9
Kimhansav <gene2002@naver.com>
2024-09-18 17:27:10 +0900

    Modify ui.py to add le1, le2 and cb widgets

--- ui.py
+++ ui.py
@@ -1,7 +1,8 @@
-#ch 5.2.1 ui.py
+#ch 6.2.1 ui.py
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout,
-                             QMessageBox, QPlainTextEdit, QHBoxLayout) #QHBoxLayout 추가
+                             QMessageBox, QPlainTextEdit, QHBoxLayout, QLineEdit, QComboBox) #QLineEdit, QComboBox 추가
 from PyQt5.QtGui import QIcon
+from PyQt5 import QtCore #모듈 추가
 
 class View(QWidget):
 
@@ -17,6 +18,20 @@ class View(QWidget):
 
         self.btn2 = QPushButton('clear', self) #버튼 2 추가
 
+        self.le1 = QLineEdit('0', self) #라인 에디트1 추가
+        self.le1.setAlignment(QtCore.Qt.AlignRight) #라인 에디트1 문자열 배치 설정
+
+        self.le2 = QLineEdit('0', self) #라인 에디트2 추가
+        self.le2.setAlignment(QtCore.Qt.AlignRight) #라인 에디트2 문자열 배치 설정
+
+        self.cb = QComboBox(self) #콤보 박스 추가
+        self.cb.addItems(['+', '-', '*', '/']) #콤보 박스 항목 추가(연산자로 사용)
+
+        hbox_formular = QHBoxLayout() #새로 정의한 위젯을 QHBoxLayout에 배치
+        hbox_formular.addWidget(self.le1)
+        hbox_formular.addWidget(self.cb)
+        hbox_formular.addWidget(self.le2)
+
         hbox = QHBoxLayout() # 수평 박스 레이아웃을 추가하고 버튼1, 2 추가
         hbox.addStretch(1) # 공백
         hbox.addWidget(self.btn1) #버튼 1 배치
@@ -26,6 +41,7 @@ class View(QWidget):
         vbox.addWidget(self.te1) #수직 레이아웃에 텍스트 에디트 위젯 추가
 
         # vbox.addWidget(self.btn1) #버튼 위치
+        vbox.addLayout(hbox_formular) #hbox_formular 배치
         vbox.addLayout(hbox) #btn1 위치에 hbox를 배치
         vbox.addStretch(1) #빈 공간
 
